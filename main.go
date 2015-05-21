4f61803f77a31df1a1ddb239882cd77ba0f7cc79
Ashwin Purohit <purohit@gmail.com>
2015-05-21 10:48:27 -0600

    Ability to unmarshal into map[string]float64

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"regexp"
+	"strconv"
 
 	"github.com/recursionpharma/ec2prices/files"
 )
@@ -63,8 +64,32 @@ type Size struct {
 }
 
 type ValueColumn struct {
-	Name string `json:"name,omitempty"`
-	//Prices map[string]float64 `json:"prices,omitempty"`
+	Name   string
+	Prices map[string]float64
+}
+
+func (v *ValueColumn) UnmarshalJSON(data []byte) error {
+	// We need to custom unmarshal this because it's a map of float64s
+	// but the values are strings in the JSON data.
+	type valueColumn struct {
+		Name   string            `json:"name,omitempty"`
+		Prices map[string]string `json:"prices,omitempty"`
+	}
+	vc := valueColumn{}
+	err := json.Unmarshal(data, &vc)
+	if err != nil {
+		return err
+	}
+	v.Name = vc.Name
+	v.Prices = make(map[string]float64, 0)
+	for key, val := range vc.Prices {
+		f, err := strconv.ParseFloat(val, 64)
+		if err != nil {
+			return err
+		}
+		v.Prices[key] = f
+	}
+	return nil
 }
 
 // GetPrices parses the JSON file for the given resource, and unmarshals it into a struct
